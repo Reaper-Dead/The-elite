8390a04b18ab40fc30cb578270e15a3001508849
Reaper-Dead <ufoalien0497@gmail.com>
2026-06-26 21:19:46 +0530

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  bot: {
+    name: "The Elite",
+    version: "1.0.0",
+    developers: ["Reaper-Dead"],
+    supportServer: "",
+    invite: ""
+  },
+
+  economy: {
+    currency: "Zeni",
+    dailyReward: 500,
+    workMin: 100,
+    workMax: 500
+  }
+};
