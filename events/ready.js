091ff9f8de0cccde735995f829889b6e66f47a7c
Reaper-Dead <ufoalien0497@gmail.com>
2026-06-26 21:22:40 +0530

    Create events/ready.js

---
+++ events/ready.js
@@ -0,0 +1,20 @@
+const { Events, ActivityType } = require("discord.js");
+
+module.exports = {
+  name: Events.ClientReady,
+  once: true,
+
+  execute(client) {
+    console.log(`✅ Logged in as ${client.user.tag}`);
+
+    client.user.setPresence({
+      activities: [
+        {
+          name: "Dragon Soul",
+          type: ActivityType.Playing
+        }
+      ],
+      status: "online"
+    });
+  }
+};
